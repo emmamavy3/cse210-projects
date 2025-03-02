0543c1faa0afd293653b11de3591721984d1e6d6
Emma Mavy <emma.mavy3@gmail.com>
2025-03-01 22:01:36 -0700

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    // static void Main(string[] args)
+    // {
+    //     Console.WriteLine("Hello Prep5 World!");
+    // }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string Name = Console.ReadLine();
+        return Name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int Number = int.Parse (Console.ReadLine());
+        return Number;
+    }
+    static int SquareNumber(int Number)
+    {
+        int square = Number* Number;
+        return square;
+    }
+
+    static void DisplayResults(int square, string Name)
+    {
+        Console.WriteLine($"{Name}, the square of your number is {square}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber();
+        int SquaredNumber = SquareNumber(UserNumber);
+
+        DisplayResults(SquaredNumber, UserName);
     }
+    
 }
