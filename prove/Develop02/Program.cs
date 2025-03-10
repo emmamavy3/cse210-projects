a7746f831402a3e5da9acd12c631e16865a51424
Emma Mavy <emma.mavy3@gmail.com>
2025-03-10 09:14:51 -0600

    menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -11,15 +11,38 @@ class Program
 
     }
 
-    // static void Menu()
-    // {
-    //     Console.WriteLine("Please select one of the following");
-    //     Console.WriteLine("1. Write");
-    //     Console.WriteLine("2. Display");
-    //     Console.WriteLine("3. Load");
-    //     Console.WriteLine("4. Save");
-    //     Console.WriteLine("5. Quit");
-    //     Console.WriteLine("What would you like to do");
+    static void Menu()
+    {
+        Console.WriteLine("Please select one of the following");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.WriteLine("What would you like to do");
+        int num = int.Parse(Console.ReadLine());
+        if (num == 1)
+        {
+            Entry myEntry = new Entry();
+            myEntry.GetPrompt();
+            myEntry.GetEntry();
+            myEntry.DisplayEntry();
+        }
+        else if (num == 2)
+        {
 
-    // }
+        }
+        else if (num == 3)
+        {
+
+        }
+        else if (num == 4)
+        {
+
+        }
+        else if (num == 5)
+        {
+            Console.WriteLine("");
+        }
+    }
 }
